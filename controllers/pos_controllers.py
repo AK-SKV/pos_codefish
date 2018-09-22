ea7508a98a265ea8a8c08fb4f98238f59e082b14
CodeFish <msetiha@gmail.com>
2018-09-22 07:35:34 +0200

    new controller py

--- controllers/pos_controllers.py
+++ controllers/pos_controllers.py
@@ -12,7 +12,6 @@ import werkzeug.utils
 
 _logger = logging.getLogger(__name__)
 
-
 class dataset(DataSet):
     @http.route('/web/dataset/search_read', type='json', auth="user")
     def search_read(self, model, fields=False, offset=0, limit=False, domain=None, sort=None):
@@ -22,7 +21,6 @@ class dataset(DataSet):
         if context.get('retail', False):
             request.env['pos.cache.database'].insert_data(datas['records'], model, True)
         return datas
-
 
 class pos_controller(PosController):
 
@@ -63,6 +61,17 @@ class pos_controller(PosController):
             bus_logs = bug_log_model.api_get_data(pos_config.bus_id.id)
         session_info['stock_datas'] = cache_model.get_on_hand_by_stock_location(
             pos_session.config_id.stock_location_id.id)
+        locations = request.env['stock.location'].search([(
+            'usage', '=', 'internal'
+        )])
+        stocks = {}
+        for location in locations:
+            datas = cache_model.get_product_available_filter_by_stock_location_ids(
+                tuple([-1, location.id]))
+            if datas:
+                stocks[location.id] = datas
+        _logger.info(stocks)
+        session_info['stocks'] = stocks
         session_info['bus_logs'] = bus_logs
         session_info['model_ids'] = {
             'product.pricelist': {},
@@ -97,24 +106,11 @@ class pos_controller(PosController):
             request.env.cr.execute("select max(id) from %s" % table)
             max_ids = request.env.cr.fetchall()
             session_info['model_ids'][object]['max_id'] = max_ids[0][0] if max_ids and max_ids[0] else 1
-            session_info['pos_cache_database'] = cache_model.load_master_data({	
-                'product.product': True,
-                'pos.order.line': True,
-                'res.partner': True,
-                'account.invoice': True,
-                'product.pricelist.item': True,
-                'sale.order.line': True,
-                'product.pricelist': True,
-                'account.invoice.line': True,
-                'sale.order': True,
-                'pos.order': True
-            })
         context = {
             'session_info': json.dumps(session_info)
         }
         _logger.info('->> end pos_web')
         return request.render('point_of_sale.index', qcontext=context)
-
 
 class web_login(Home):
     @http.route()
@@ -127,7 +123,6 @@ class web_login(Home):
             if pos_config:
                 return http.local_redirect('/pos/web/')
         return response
-
 
 class pos_bus(BusController):
     def _poll(self, dbname, channels, last, options):
@@ -145,8 +140,8 @@ class pos_bus(BusController):
     @http.route('/pos/sync', type="json", auth="public")
     def send(self, bus_id, messages):
         for message in messages:
-            if not message.get('value', None) or not message['value'].get('order_uid', None) or not \
-                    message['value'].get('action', None):
+            if not message.get('value', None) or not message['value'].get('order_uid', None) or not message[
+                'value'].get('action', None):
                 continue
             order_uid = message['value'].get('order_uid')
             action = message['value'].get('action')
